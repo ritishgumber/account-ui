b8b90f75d531f672864e5debad1df8ffc0a35cf4
shubhamqweasd <shubhamqweasd@gmail.com>
2017-02-27 10:45:13 +0530

    company details

--- app/components/register.js
+++ app/components/register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router'
 import axios from 'axios'
 import CircularProgress from 'material-ui/CircularProgress'
+import DOMAIN_LIST from './domains'
 
 class Register extends React.Component {
    constructor(){
@@ -12,7 +13,13 @@ class Register extends React.Component {
          name:'',
          email:'',
          password:'',
-         progress:false
+         progress:false,
+         isCustomDomain:false,
+         companyName:'',
+         companySize:'1-10',
+         phoneNumber:'',
+         reference:'',
+         jobRole:'executive'
       }
    }
    componentDidMount(){
@@ -25,7 +32,13 @@ class Register extends React.Component {
    signUp(e){
       e.preventDefault()
       this.setProgress(true)
-      let postData = {email:this.state.email,password:this.state.password,name:this.state.name,isAdmin:false}
+      let postData = {}
+      if(this.state.isCustomDomain){
+        postData = {email:this.state.email,password:this.state.password,name:this.state.name,isAdmin:false,companyName:this.state.companyName,companySize:this.state.companySize,phoneNumber:this.state.phoneNumber,reference:this.state.reference,jobRole:this.state.jobRole}
+      } else {
+        postData = {email:this.state.email,password:this.state.password,name:this.state.name,isAdmin:false}
+      }
+      
       axios.post(USER_SERVICE_URL+"/user/signup",postData).then(function(data){
          if(!__isDevelopment){
             /****Tracking*********/                
@@ -35,7 +48,12 @@ class Register extends React.Component {
              //mixpanel.identify(data._id);
 
              mixpanel.register({ "Name": this.state.name,"Email": this.state.email});
-             mixpanel.track('Signup', { "Name": this.state.name,"Email": this.state.email});
+            if(this.state.isCustomDomain){
+              mixpanel.track('Signup', { "Name": this.state.name,"Email": this.state.email,"CompanyName": this.state.companyName,"CompanySize": this.state.companySize,"PhoneNumber": this.state.phoneNumber,"JobRole": this.state.jobRole});
+            } else {
+              mixpanel.track('Signup', { "Name": this.state.name,"Email": this.state.email});
+            }
+             
             /****End of Tracking*****/
          }
          this.setProgress(false) 
@@ -58,6 +76,14 @@ class Register extends React.Component {
              mixpanel.track('Portal:Clicked SignUp Button', { "Clicked": "SignUp Button in portal!"});
             /****End of Tracking*****/
           }
+   }
+   validateEmail(e){
+      e.preventDefault()
+      let domain = this.state.email.replace(/.*@/, "")
+      let isCustomDomain = DOMAIN_LIST.indexOf(domain) === -1
+      if(isCustomDomain){
+        this.setState({isCustomDomain:isCustomDomain})
+      } else this.signUp()
    }
    changeHandler(which,e){
       this.state[which] = e.target.value
@@ -84,17 +110,37 @@ class Register extends React.Component {
                   <h3>We've sent you the verification email.</h3>
                </div>
          		<div id="box" className={this.state.success ? 'hide':''}>
-         			<h5 className="tacenter bfont">Sign up free, no card required.</h5>
+         			<h5 className={!this.state.isCustomDomain ? 'tacenter bfont':'hide'}>Sign up free, no card required.</h5>
+               <h5 className={this.state.isCustomDomain ? 'tacenter bfont':'hide'}>One last step.</h5>
          		</div>
                <div id="box" className={!this.state.success ? 'hide':''}>
                   <h5 className="tacenter bfont">We have sent you the verification email. Please make sure you check spam.</h5>
                </div>
          		<div id="loginbox" className={!this.state.success ? '':'hide'}>
                   <h5 className="tacenter red">{ this.state.errorMessage }</h5>
-                  <form onSubmit={this.signUp.bind(this)}>
-            			<input type="text" value={this.state.name} onChange={this.changeHandler.bind(this,'name')} className="loginInput from-control" placeholder="Full Name" required/>
+                  <form onSubmit={this.validateEmail.bind(this)} className={!this.state.isCustomDomain ? '':'hide'}>
+            			   <input type="text" value={this.state.name} onChange={this.changeHandler.bind(this,'name')} className="loginInput from-control" placeholder="Full Name" required/>
                      <input type="email" value={this.state.email} onChange={this.changeHandler.bind(this,'email')} className="loginInput from-control" placeholder="Email" required/>
                      <input type="password" value={this.state.password} onChange={this.changeHandler.bind(this,'password')} className="loginInput from-control" placeholder="Password" required/>
+            			<button className="loginbtn"  type="submit"> Sign up for CloudBoost </button>
+                  </form>
+                  <form onSubmit={this.signUp.bind(this)} className={this.state.isCustomDomain ? '':'hide'}>
+            			   <input type="text" value={this.state.companyName} onChange={this.changeHandler.bind(this,'companyName')} className="loginInput from-control" placeholder="Company Name" required/>
+                     <input type="number" value={this.state.phoneNumber} onChange={this.changeHandler.bind(this,'phoneNumber')} className="loginInput from-control" placeholder="Phone Number" required/>
+                     <select className="companysize" required value={this.state.companySize} onChange={this.changeHandler.bind(this,'companySize')}>
+                       <option value="1-10">Company Size - 1-10</option>
+                       <option value="11-50">Company Size - 11-50</option>
+                       <option value="50-200">Company Size - 50-200</option>
+                       <option value="200-1000">Company Size - 200-1000</option>
+                       <option value="1000+">Company Size - 1000+</option>
+                     </select>
+                     <select className="companysize" required value={this.state.jobRole} onChange={this.changeHandler.bind(this,'jobRole')}>
+                       <option value="executive">Job Role - Executive</option>
+                       <option value="vp">Job Role - VP</option>
+                       <option value="projectManager">Job Role - Project Manager</option>
+                       <option value="developer">Job Role - Developer</option>
+                     </select>
+                     <input type="text" value={this.state.reference} onChange={this.changeHandler.bind(this,'reference')} className="loginInput from-control" placeholder="How did you hear about us ?" required/>
             			<button className="loginbtn"  type="submit"> Sign up for CloudBoost </button>
                   </form>
          		</div>
